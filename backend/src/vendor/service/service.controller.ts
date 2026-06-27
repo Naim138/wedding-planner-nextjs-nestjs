c825409e64fd8f9dc4714b0a2aacb2586076b0d4
Naim138 <ug2102050@cse.pstu.ac.bd>
2026-06-28 01:47:21 +0600

    Remove AuthGuard from category endpoint to make it public

--- backend/src/vendor/service/service.controller.ts
+++ backend/src/vendor/service/service.controller.ts
@@ -25,7 +25,6 @@ export class ServiceController {
         return res_obj
     }
     @Get("/category")
-    @UseGuards(AuthGuard)
     async getAllCategories(){
         const res_obj = await this.vendorSevice.getAllCategoriesService();
         return res_obj
